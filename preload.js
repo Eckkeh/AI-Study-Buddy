8b33ea173e37cc6ef853e8e5fdbb95e7af6247cc
Rebekah Barts <rabarts@student.fullsail.edu>
2025-06-07 07:57:33 -0400

    Expose sendPdf function via preload bridge

--- preload.js
+++ preload.js
@@ -2,4 +2,5 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('api', {
     sendText: (text) => ipcRenderer.invoke('send-text', text),
+    sendPdf: (pdfBytes) => ipcRenderer.invoke('send-pdf', pdfBytes),
 });
